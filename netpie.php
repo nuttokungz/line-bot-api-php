4e2f1bb77f1c793749631625c2be91b34992a9a3
nuttokungz <53294361+nuttokungz@users.noreply.github.com>
2019-07-26 09:52:40 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "NuttoIoT/"; //enter your appid
+		$KEY = "7KLqKLtBMRUpG57"; //enter your web key
+		$SECRET = "xwWbhVQgQ9gyhy9W1m4RaAVMk"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
